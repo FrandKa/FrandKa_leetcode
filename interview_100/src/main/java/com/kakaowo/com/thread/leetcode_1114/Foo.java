624a49c53bcfd12a8169f586036638f938bc79ee
Frand ka kaslana <Frandkytj@outlook.com>
2023-11-10 22:38:33 +0800

    leetcode 1114

--- interview_100/src/main/java/com/kakaowo/com/thread/leetcode_1114/Foo.java
+++ interview_100/src/main/java/com/kakaowo/com/thread/leetcode_1114/Foo.java
@@ -1,42 +1,34 @@
 package com.kakaowo.com.thread.leetcode_1114;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.CountDownLatch;
 
 class Foo {
-    private final Lock lock = new ReentrantLock();
-    private final Condition Condition1 = lock.newCondition();
-    private final Condition Condition2 = lock.newCondition();
-    private final Condition Condition3 = lock.newCondition();
-
-    private AtomicInteger atomicIntegerFirst = new AtomicInteger(0);
-    private AtomicInteger atomicIntegerSecond = new AtomicInteger(0);
+    private CountDownLatch countDownLatchFirst = new CountDownLatch(1);
+    private CountDownLatch countDownLatchSecond = new CountDownLatch(1);
 
     public Foo() {
-        
+
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        
+
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        atomicIntegerFirst.incrementAndGet();
+        countDownLatchFirst.countDown();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        
+
         // printSecond.run() outputs "second". Do not change or remove this line.
-        while(atomicIntegerFirst.get() != 1) {}
+        countDownLatchFirst.await();
         printSecond.run();
-        atomicIntegerSecond.incrementAndGet();
+        countDownLatchSecond.countDown();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        
+
         // printThird.run() outputs "third". Do not change or remove this line.
-        while(atomicIntegerSecond.get() != 1) {}
+        countDownLatchSecond.await();
         printThird.run();
     }
 }
